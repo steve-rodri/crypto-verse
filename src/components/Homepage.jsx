59cc7aab3c1a0db5c9acb5a17b4c6178612c9640
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-02-08 14:06:36 -0500

    implement redux, add global crypto stats

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import millifiy from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
+const { Title } = Typography;
 
 const Homepage = () => {
-  return <div className="Homepage"></div>;
+  const { data, isFetching } = useGetCryptosQuery();
+  const globalStats = data?.data?.stats;
+  if (isFetching) return "Loading...";
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Exchanges"
+            value={millifiy(globalStats.totalExchanges)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Market Cap"
+            value={millifiy(globalStats.totalMarketCap)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total 24h Volume"
+            value={millifiy(globalStats.total24hVolume)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Markets"
+            value={millifiy(globalStats.totalMarkets)}
+          />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Homepage;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "ad2ece6223msh5d8d8dd4b4b7a70p18e07fjsnf80ab9df97de",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = url => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "crypto",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: builder => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
+
+// const options = {
+//   method: "GET",
+//   url:
+//   params: {
+//     referenceCurrencyUuid: "yhjMzLPhuIDl",
+//     limit: "50",
+//     offset: "0",
+//     orderBy: "24hVolume",
+//     orderDirection: "desc",
+//   },
+// };
