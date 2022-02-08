b9513b03e5bcd3ae61103b8144941933b37640b9
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-02-08 17:49:08 -0500

    refactor Cryptocurrencies.jsx

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -20,38 +20,46 @@ const Cryptocurrencies = ({ simplified }) => {
   return (
     <>
       {!simplified && (
-        <div className="search-crypto">
-          <Input
-            placeholder="Search Cryptocurrencies"
-            onChange={e => setSearchTerm(e.target.value)}
-          />
-        </div>
+        <SearchField
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
       )}
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map(currency => (
-          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-            <Link to={`/cryptocurrencies/${currency.id}`}>
-              <Card
-                title={`${currency.rank} ${currency.name}`}
-                hoverable
-                extra={
-                  <img
-                    className="crypto-image"
-                    src={currency.iconUrl}
-                    alt={currency.name}
-                  />
-                }
-              >
-                <p>Price: {millify(currency.price)}</p>
-                <p>Market Cap: {millify(currency.marketCap)}</p>
-                <p>Daily Change: {millify(currency.change)}%</p>
-              </Card>
-            </Link>
-          </Col>
+          <CurrencyCard currency={currency} />
         ))}
       </Row>
     </>
   );
 };
+
+const SearchField = props => (
+  <div className="search-crypto">
+    <Input placeholder="Search Cryptocurrencies" {...props} />
+  </div>
+);
+
+const CurrencyCard = ({ currency }) => (
+  <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
+    <Link to={`/cryptocurrencies/${currency.symbol}`}>
+      <Card
+        title={`${currency.rank} ${currency.name}`}
+        hoverable
+        extra={
+          <img
+            className="crypto-image"
+            src={currency.iconUrl}
+            alt={currency.name}
+          />
+        }
+      >
+        <p>Price: ${millify(currency.price)}</p>
+        <p>Market Cap: ${millify(currency.marketCap)}</p>
+        <p>Daily Change: {millify(currency.change)}%</p>
+      </Card>
+    </Link>
+  </Col>
+);
 
 export default Cryptocurrencies;
