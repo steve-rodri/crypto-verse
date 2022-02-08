e13d47efee8f603fe968729722ddea26d572f444
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-02-08 14:31:00 -0500

    implement Cryptocurrencies component/page

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const CryptoDetails = () => {
+  const [data: cryptosList, isFetching] = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  console.log(cryptos);
   return <div className="CryptoDetails"></div>;
 };
 
