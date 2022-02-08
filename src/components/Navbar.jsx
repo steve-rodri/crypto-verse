a2d6cb33798efdfd517e738998d980cfa608117a
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-02-08 12:10:07 -0500

    install packages, add app styles, start coding navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons/lib/icons";
+import icon from "../images/cryptocurrency.png";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Cryptoverse</Link>
+        </Typography.Title>
+        <Button></Button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
