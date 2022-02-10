be2f941b970965e9b91e25d73e3e7ee567089009
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-02-09 23:16:25 -0500

    refactor Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button, Menu, Typography, Avatar } from "antd";
+import { Button, Menu as AntMenu, Typography, Avatar } from "antd";
 import { Link } from "react-router-dom";
 import {
   HomeOutlined,
@@ -10,9 +10,13 @@ import {
 } from "@ant-design/icons/lib/icons";
 import icon from "../images/cryptocurrency.png";
 
+const { Title } = Typography;
+const { Item } = AntMenu;
+
 const Navbar = () => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [screenSize, setScreenSize] = useState(null);
+
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
     window.addEventListener("resize", handleResize);
@@ -24,38 +28,42 @@ const Navbar = () => {
     if (screenSize < 768) setActiveMenu(false);
     else setActiveMenu(true);
   }, [screenSize]);
+
   return (
     <div className="nav-container">
       <div className="logo-container">
         <Avatar src={icon} size="large" />
-        <Typography.Title level={2} className="logo">
+        <Title level={2} className="logo">
           <Link to="/">Cryptoverse</Link>
-        </Typography.Title>
-        <Button
-          className="menu-control-container"
-          onClick={() => setActiveMenu(!activeMenu)}
-        >
-          <MenuOutlined />
-        </Button>
+        </Title>
+        <HamburgerButton onClick={() => setActiveMenu(!activeMenu)} />
       </div>
-      {activeMenu && (
-        <Menu theme="dark">
-          <Menu.Item icon={<HomeOutlined />} key={1}>
-            <Link to="/">Home</Link>
-          </Menu.Item>
-          <Menu.Item icon={<FundOutlined />} key={2}>
-            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-          </Menu.Item>
-          <Menu.Item icon={<MoneyCollectOutlined />} key={3}>
-            <Link to="/exchanges">Exchanges</Link>
-          </Menu.Item>
-          <Menu.Item icon={<BulbOutlined />} key={4}>
-            <Link to="/news">News</Link>
-          </Menu.Item>
-        </Menu>
-      )}
+      {activeMenu && <Menu />}
     </div>
   );
 };
+
+const HamburgerButton = ({ onClick }) => (
+  <Button className="menu-control-container" onClick={onClick}>
+    <MenuOutlined />
+  </Button>
+);
+
+const Menu = () => (
+  <AntMenu theme="dark">
+    <Item icon={<HomeOutlined />} key={1}>
+      <Link to="/">Home</Link>
+    </Item>
+    <Item icon={<FundOutlined />} key={2}>
+      <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+    </Item>
+    <Item icon={<MoneyCollectOutlined />} key={3}>
+      <Link to="/exchanges">Exchanges</Link>
+    </Item>
+    <Item icon={<BulbOutlined />} key={4}>
+      <Link to="/news">News</Link>
+    </Item>
+  </AntMenu>
+);
 
 export default Navbar;
