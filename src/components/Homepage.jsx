032d5d31086a369274612dd1a5b2addee9e76576
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-02-09 23:36:18 -0500

    refactor Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,69 +2,92 @@ import React from "react";
 import millifiy from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
-import { useGetCryptosQuery } from "../services/cryptoApi";
+import { useGetCryptosQuery } from "../services";
 import { Cryptocurrencies, News, Loader } from "../components";
 
 const { Title } = Typography;
 
 const Homepage = () => {
+  return (
+    <>
+      <GlobalCryptoStats />
+      <Top10Cryptos />
+      <LatestNews />
+    </>
+  );
+};
+
+const GlobalCryptoStats = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
-  const globalStats = data?.data?.stats;
-  if (isFetching) return <Loader />;
+  const stats = data?.data?.stats;
   return (
     <>
       <Title level={2} className="heading">
         Global Crypto Stats
       </Title>
-      <Row>
-        <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
-        </Col>
-        <Col span={12}>
-          <Statistic
-            title="Total Exchanges"
-            value={millifiy(globalStats.totalExchanges)}
-          />
-        </Col>
-        <Col span={12}>
-          <Statistic
-            title="Total Market Cap"
-            value={millifiy(globalStats.totalMarketCap)}
-          />
-        </Col>
-        <Col span={12}>
-          <Statistic
-            title="Total 24h Volume"
-            value={millifiy(globalStats.total24hVolume)}
-          />
-        </Col>
-        <Col span={12}>
-          <Statistic
-            title="Total Markets"
-            value={millifiy(globalStats.totalMarkets)}
-          />
-        </Col>
-      </Row>
-      <div className="home-heading-container">
-        <Title level={2} className="home-title">
-          Top 10 Cryptocurrencies in the world
-        </Title>
-        <Title level={3} className="show-more">
-          <Link to="/cryptocurrencies">Show More</Link>
-        </Title>
-      </div>
-      <Cryptocurrencies simplified />
-      <div className="home-heading-container">
-        <Title level={2} className="home-title">
-          Latest Crypto News
-        </Title>
-        <Title level={3} className="show-more">
-          <Link to="/news">Show More</Link>
-        </Title>
-      </div>
-      <News simplified />
+      {isFetching ? (
+        <Loader />
+      ) : (
+        <Row>
+          <Col span={12}>
+            <Statistic title="Total Cryptocurrencies" value={stats.total} />
+          </Col>
+          <Col span={12}>
+            <Statistic
+              title="Total Exchanges"
+              value={millifiy(stats.totalExchanges)}
+            />
+          </Col>
+          <Col span={12}>
+            <Statistic
+              title="Total Market Cap"
+              value={millifiy(stats.totalMarketCap)}
+            />
+          </Col>
+          <Col span={12}>
+            <Statistic
+              title="Total 24h Volume"
+              value={millifiy(stats.total24hVolume)}
+            />
+          </Col>
+          <Col span={12}>
+            <Statistic
+              title="Total Markets"
+              value={millifiy(stats.totalMarkets)}
+            />
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
+
+const Top10Cryptos = () => (
+  <>
+    <div className="home-heading-container">
+      <Title level={2} className="home-title">
+        Top 10 Cryptocurrencies in the world
+      </Title>
+      <Title level={3} className="show-more">
+        <Link to="/cryptocurrencies">Show More</Link>
+      </Title>
+    </div>
+    <Cryptocurrencies simplified />
+  </>
+);
+
+const LatestNews = () => (
+  <>
+    <div className="home-heading-container">
+      <Title level={2} className="home-title">
+        Latest Crypto News
+      </Title>
+      <Title level={3} className="show-more">
+        <Link to="/news">Show More</Link>
+      </Title>
+    </div>
+    <News simplified />
+  </>
+);
 
 export default Homepage;
